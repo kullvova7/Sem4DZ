e5aee13d36bf6a2d9e81a663348e4f58a87cb7a8
Vladimir <Vlad>
2024-02-28 11:03:58 +0300

    commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿void Main()
+{
+    while (true)
+    {
+        Console.Write("Введите целое число или 'q' для выхода: ");
+        string input = Console.ReadLine();
+
+        if (input.ToLower() == "q")
+        {
+            Console.WriteLine("Программа завершена.");
+            break;
+        }
+
+        if (IsSumOfDigitsEven(Convert.ToInt32(input)))
+        {
+            Console.WriteLine($"Сумма цифр числа {input} чётная. Программа завершена.");
+            break;
+        }
+    }
+}
+
+static bool IsSumOfDigitsEven(int number)
+{
+    int sum = 0;
+    foreach (char digitChar in number.ToString())
+    {
+        int digit = int.Parse(digitChar.ToString());
+        sum += digit;
+    }
+    return sum % 2 == 0;
+}
+Main();
